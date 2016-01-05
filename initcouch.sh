6b97438a04bf8fe48c4b1217fa7dfc11de842b54
Pedro Teixeira <i@pgte.me>
2016-01-05 15:36:14 +0000

    no user, even if admin, can access the main database

--- initcouch.sh
+++ initcouch.sh
@@ -10,7 +10,7 @@ curl -X PUT $SECUREHOST/_users/_security -d '{ "admins": { "names": [], "roles":
 curl -X PUT $SECUREHOST/config
 curl -X PUT $SECUREHOST/config/_security -d '{ "admins": { "names": [], "roles": ["admin"]}, "members": { "names": [], "roles": []}}'
 curl -X PUT $SECUREHOST/main
-curl -X PUT $SECUREHOST/main/_security -d '{ "admins": { "names": [], "roles": ["admin"]}, "members": { "names": [], "roles": ["user"]}}'
+curl -X PUT $SECUREHOST/main/_security -d '{ "admins": { "names": [], "roles": ["admin"]}, "members": { "names": [], "roles": []}}'
 curl -X PUT $SECUREHOST/_config/http/authentication_handlers -d '"{couch_httpd_oauth, oauth_authentication_handler}, {couch_httpd_auth, proxy_authentification_handler}, {couch_httpd_auth, cookie_authentication_handler}, {couch_httpd_auth, default_authentication_handler}"'
 curl -X PUT $SECUREHOST/_config/couch_httpd_oauth/use_users_db -d '"true"'
 curl -X PUT $SECUREHOST/_users/org.couchdb.user:hradmin -d '{"name": "hradmin", "password": "test", "roles": ["System Administrator","admin","user"], "type": "user", "userPrefix": "p1"}'
